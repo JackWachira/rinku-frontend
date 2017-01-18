8f83736e93fb406a5449191d6f1d633100954f49
Amos Omondi <amos.omondi@andela.com>
2017-01-18 13:44:40 +0300

    (chore) add primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { PanelModule } from 'primeng/primeng';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
