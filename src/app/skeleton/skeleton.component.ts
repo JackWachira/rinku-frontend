b94642085ba5b483a369dafeb82c6bdd8fd61829
Amos Omondi <amos.omondi@andela.com>
2017-02-09 14:41:21 +0300

    (chore) get user name, avatar and channel name

--- src/app/skeleton/skeleton.component.ts
+++ src/app/skeleton/skeleton.component.ts
@@ -9,6 +9,10 @@ import { LinksService } from '../links/links.service';
 })
 export class Skeleton implements OnInit {
   links: any;
+  userAvatar: string;
+  userName: string;
+  channelNames: Array<String>;
+
   constructor(private linksService: LinksService
   ) { }
 
@@ -26,12 +30,34 @@ export class Skeleton implements OnInit {
   }
 
   ngOnInit(): void {
-    let token = localStorage.getItem('rinku');
-    this.linksService.getLinks(token['team_id']).subscribe(
-      links => {
-        this.links = links;
-      },
-      error => console.log(error)
-    )
+    let self = this;
+    let names = [];
+    
+    let token = JSON.parse(localStorage.getItem('rinku'));
+
+    const getLinks = function() {
+      self.linksService.getLinks(token.team.id).subscribe(
+        links => {
+          self.links = links;
+
+          // This is to make sure channel names are not repeated
+          self.links.map((link) => {
+            if(names.indexOf(link.channel_name) === -1) {
+              names.push(link.channel_name);
+            }
+          });
+          self.channelNames = names;
+        },
+        error => console.log(error)
+      )
+    }
+
+    if (token) {    
+      if(token.ok) {
+        self.userAvatar = token.user.image_32;
+        self.userName = token.user.name;
+        getLinks();
+      }
+    }
   }
 }
