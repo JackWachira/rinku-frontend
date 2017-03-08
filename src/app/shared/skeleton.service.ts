bfa2935bdc336f800ec9f0796597e5a84778b4d4
Jack Mwangi <jack.wachira@andela.com>
2017-03-08 14:48:02 +0300

    Add search bar

--- src/app/shared/skeleton.service.ts
+++ src/app/shared/skeleton.service.ts
@@ -4,14 +4,22 @@ import { ChannelItem } from './channel-item';
 @Injectable()
 export class SkeletonService {
     public channelChanged$: EventEmitter<ChannelItem>;
+    public queryLinks$: EventEmitter<string>;
 
     constructor() {
         this.channelChanged$ = new EventEmitter<ChannelItem>();
+        this.queryLinks$ = new EventEmitter<string>();
     }
     getChannelEmitter() {
         return this.channelChanged$;
     }
     public changeChannel(item: ChannelItem): void {
         this.channelChanged$.emit(item);
+    }
+    getSearchEmitter() {
+        return this.queryLinks$;
+    }
+    public searchLinks(query: string): void {
+        this.queryLinks$.emit(query);
     }
 }
